31292939d8d693c5182f6986e48d8f8c662cf8a7
MarcGalvez1 <marc.galvez83@gmail.com>
2023-11-19 19:29:56 -0500

    set up setFullName it doesn't work just yet

--- src/MyPerson.java
+++ src/MyPerson.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
+
 public class MyPerson {
+
+    Scanner scnr = new Scanner(System.in);
+
     // Initialize private fields
     private String firstName = "John";
     private String lastName = "Doe";
@@ -8,15 +14,37 @@ public class MyPerson {
     protected static int baseIDNumber = 100000;
 
     public MyPerson() {
-        idNumber = setIDNumber();
+        //idNumber = setIDNumber();
 
     }
 
     public MyPerson(String fullName) {
-        idNumber = setIDNumber();
+        //idNumber = setIDNumber();
         setFullName(fullName);
     }
 
+    public void setFullName(String name) {
+        String fName;
+        String lName;
+        if ((name.indexOf(" ") == name.lastIndexOf(" ")) && name.contains(" ") && !name.isBlank()) {
+            fName = scnr.next(name);
+            lName = scnr.next(name);
+            setFirstName(fName);
+            setLLastName(lName);
+        }
+        else {
+            if (name.isBlank()) {
+                System.out.println("ERROR: Full Name must cannot be blank.");
+            }
+            else if (!name.contains(" ")) {
+                System.out.println("ERROR: Full Name must have a space between 1st and last name.");
+            }
+            else if (name.indexOf(" ") != name.lastIndexOf(" ")) {
+                System.out.println("ERROR: Full Name must cannot contain more than 1 space.");
+            }
+
+        }
+    }
     public void setFirstName(String name) {
         if (!name.isEmpty() && !name.contains(" ")) {
             firstName = name;
@@ -25,7 +53,7 @@ public class MyPerson {
             if (name.isEmpty()) {
                 System.out.println("ERROR: First Name cannot be blank.");
             } else if (name.contains(" ")) {
-                System.out.println("\"ERROR: First name cannot contain a space.");
+                System.out.println("ERROR: First name cannot contain a space.");
             }
         }
     }
@@ -42,5 +70,7 @@ public class MyPerson {
             }
         }
     }
+
+
 
 }
