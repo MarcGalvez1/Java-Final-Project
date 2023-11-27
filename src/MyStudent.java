489043fd1d9ee41410dbf2a4eff5e7412c8cda79
MarcGalvez1 <marc.galvez83@gmail.com>
2023-11-27 12:49:16 -0500

    Created constructors

--- src/MyStudent.java
+++ src/MyStudent.java
@@ -1,2 +1,17 @@
 public class MyStudent extends MyPerson{
+    private int startYear = 2018;
+    private  double earnedGPA = 3.0;
+
+    public MyStudent() {
+        setIDNumber();
+    }
+
+    public MyStudent(String fullName, int startYear, double earnedGPA) {
+        setFullName(fullName);
+        setStartYear(startYear);
+        setEarnedGPA(earnedGPA);
+    }
+
+
+
 }
