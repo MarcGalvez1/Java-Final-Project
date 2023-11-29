8b74185fbc069d6de13fec4fc2a22e1949e1961c
MarcGalvez1 <marc.galvez83@gmail.com>
2023-11-29 00:50:17 -0500

    created output

--- src/Main.java
+++ src/Main.java
@@ -44,26 +44,61 @@ public class Main {
 
         // Code below here to be written by students
 
+        // Variables
+        int bCarsonHireYear;
+        double bCarsonBaseSalary;
+
         // Enter Billy Carson's data (using setters for
         //  first name, last name, hire year, and base salary)
+        bCarson.setFirstName("Billy");
+        bCarson.setLastName("Carson");
+        System.out.println("Enter Billy Carson's hire year: ");
+        bCarsonHireYear = scnr.nextInt();
+        bCarson.setHiredYear(bCarsonHireYear);
+        System.out.println("Enter Billy Carson's base salary: ");
+        bCarsonBaseSalary = scnr.nextDouble();
+        bCarson.setBaseSalary(bCarsonBaseSalary);
 
         // Update Julie Evans to Julie Randall (she got married)
+        jEvans.setLastName("Randall");
 
         // Add the first 10 students to CPT 234 (in the order listed above)
         // starting with Paul Jones and ending with Julie (Evans) Randall
-
+        CPT234.addStudent(pJones);
+        CPT234.addStudent(rReeves);
+        CPT234.addStudent(mAllen);
+        CPT234.addStudent(wLevi);
+        CPT234.addStudent(aSmith);
+        CPT234.addStudent(bKennedy);
+        CPT234.addStudent(jGilbert);
+        CPT234.addStudent(zBooth);
+        CPT234.addStudent(sSavoca);
+        CPT234.addStudent(jEvans);
         // Add the last 10 student to IST 222 (in the order listed above)
         // starting with Bob Kennedy and ending with Frank DeLeo
-
+        IST222.addStudent(bKennedy);
+        IST222.addStudent(jGilbert);
+        IST222.addStudent(zBooth);
+        IST222.addStudent(sSavoca);
+        IST222.addStudent(jEvans);
+        IST222.addStudent(nMcMillian);
+        IST222.addStudent(cPhilo);
+        IST222.addStudent(jQueen);
+        IST222.addStudent(dCiao);
+        IST222.addStudent(fDeLeo);
         // Display the CPT 234 info
+        CPT234.printAll();
+        CPT234.printCourseList();
 
         // Display the IST 222 info
+        IST222.printAll();
+        IST222.printCourseList();
 
         // Display Billy Carson's info
-
+        bCarson.printAll();
         // Display CPT 234 student with earliest start year
-
+        CPT234.printMinYear();
         // Display IST 222 student with highest GPA
-
+        IST222.printMaxGpa();
     }
 }
